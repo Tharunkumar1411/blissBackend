de6302cb38c951470f173bf65670fb223dd029e9
Tharunkumar G <tharunkumar.govindharaj@getshopse.com>
2025-05-11 02:00:17 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const router = require('./route/index');
+const router = require('./src/route/index');
 
 // Load environment variables
 dotenv.config();
